da46f36b032f314c58c2a7dded001796cb8568b2
Jon Herbst <herbtaculargames@gmail.com>
2021-11-16 18:53:06 +0000

    Start working with small state machine to decide what actions we want to take during different sections of the file to be read

--- parser/read.py
+++ parser/read.py
@@ -3,12 +3,38 @@ import sys
 class FileReader:
     def __init__(self, filename=None):
         self.filename = filename
+        self.state = 'init'
 
     def start(self):
         if self.filename:
             try:
-                with open(self.filename, 'r') as line:
-                    print(line)
+                with open(self.filename, 'r') as export_file:
+                    self.state = 'headers'
+
+                    for line in export_file:
+                        if 'headers' == self.state:
+                            if self.shouldChangeState(line):
+                                self.state = 'patterns'
+
+                                continue
+                            else:
+                                continue
+                        elif 'patterns' == self.state:
+                            if self.shouldChangeState(line):
+                                self.state = 'rows'
+
+                                continue
+                            else:
+                                continue
+                        elif 'rows' == self.state:
+                            if self.shouldChangeState(line):
+                                self.state = 'eof'
+
+                                continue
+                            else:
+                                continue
+                        elif 'eof' == self.state:
+                            break
             except OSError as ex:
                 """Terminate if an invalid path has been provided."""
                 sys.stdout.write(ex.strerror + '\n')
@@ -17,3 +43,21 @@ class FileReader:
                 """Any uncaught errors should still result in the application closing."""
                 sys.stdout.write('Invalid file provided. Terminating...\n')
                 sys.exit()
+
+    def shouldChangeState(self, next_line=''):
+        """
+        Define what part of the FamiTracker export
+        file we are currently reading to allow
+        for specific actions and functions
+        to be called.
+        
+        Args:
+            next_line (String): The next line in the .txt file to read. Defaults to ''.
+
+        Returns:
+            boolean: True if we want to switch state, otherwise False
+        """
+        if next_line.startswith(('COLUMNS', 'PATTERN 00', '#End of export')):
+            return True
+
+        return False
