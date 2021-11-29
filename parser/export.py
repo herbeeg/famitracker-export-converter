24bc2db74e57062933b22ee28d7b2dabf1f43244
Jon Herbst <herbtaculargames@gmail.com>
2021-11-29 20:04:48 +0000

    Prepend the project path to force config files into the exp directory for storage

--- parser/export.py
+++ parser/export.py
@@ -1,11 +1,15 @@
 import sys
 
+from os import path
+
+from utils import getRootPath
+
 class DataExporter:
     def __init__(self, timestamp=0):
         if 0 < timestamp:
             self.filenames = [
-                'ft2vis_json_' + str(timestamp),
-                'ft2vis_csv_' + str(timestamp)
+                path.join(getRootPath(), '') + 'exp/' + 'ft2vis_json_{0}.json'.format(timestamp),
+                path.join(getRootPath(), '') + 'exp/' + 'ft2vis_csv_{0}.csv'.format(timestamp)
             ]
 
         self.state = 'init'
@@ -20,7 +24,7 @@ class DataExporter:
             except Exception as ex:
                 """Any uncaught errors should still result in the application closing."""
                 sys.stdout.write('Error encountered during file reading. Terminating...\n')
-                sys.exit()        
+                sys.exit()
 
     def exportConfig(self):
         return
