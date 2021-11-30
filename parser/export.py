60ad7d5293f2f3e63ad7542dea8e5df061c7d933
Jon Herbst <herbtaculargames@gmail.com>
2021-11-30 14:01:27 +0000

    Add more elegant exception handling during the JSON config and CSV data exports for more information to the application user

--- parser/export.py
+++ parser/export.py
@@ -8,8 +8,8 @@ class DataExporter:
     def __init__(self, timestamp=0):
         if 0 < timestamp:
             self.filenames = [
-                path.join(getRootPath(), '') + 'exp/' + 'ft2vis_json_{0}.json'.format(timestamp),
-                path.join(getRootPath(), '') + 'exp/' + 'ft2vis_csv_{0}.csv'.format(timestamp)
+                path.join(getRootPath(), '') + 'exp/' + 'ft2vis_json_{time}.json'.format(time=timestamp),
+                path.join(getRootPath(), '') + 'exp/' + 'ft2vis_csv_{time}.csv'.format(time=timestamp)
             ]
 
         self.state = 'init'
@@ -21,13 +21,45 @@ class DataExporter:
                     file_contents = self.exportConfig()
                     json_file.write(file_contents)
                     json_file.close()
+            except TypeError as ex:
+                """We expect a str to be passed for writing."""
+                sys.stdout.write('{error}: Attempted to write invalid data type.\n'.format(error=str(type(ex))))
+                sys.exit()
             except Exception as ex:
                 """Any uncaught errors should still result in the application closing."""
-                sys.stdout.write('Error encountered during file reading. Terminating...\n')
+                sys.stdout.write('Error encountered during file reading.\n')
                 sys.exit()
+            finally:
+                if 'eof_json' != self.state:
+                    """Terminate the application if we don't read all necessary JSON data."""
+                    sys.stdout.write('Didn\'t reach end of JSON config correctly. Terminating...\n')
+                    sys.exit()
+
+        if self.filenames[1]:
+            try:
+                with open(self.filenames[1], 'w+') as csv_file:
+                    file_contents = self.exportData()
+                    csv_file.write(file_contents)
+                    csv_file.close()
+            except TypeError as ex:
+                """We expect a str to be passed for writing."""
+                sys.stdout.write('{error}: Attempted to write invalid data type.\n'.format(error=str(type(ex))))
+                sys.exit()
+            except Exception as ex:
+                """Any uncaught errors should still result in the application closing."""
+                sys.stdout.write('Error encountered during file reading.\n')
+                sys.exit()
+            finally:
+                if 'eof' != self.state:
+                    """Terminate the application if we don't read entire temporary data file."""
+                    sys.stdout.write('Didn\'t complete parsing of temp data file. Terminating...\n')
+                    sys.exit()
+
+        sys.stdout.write('Succesfully wrote config to {path} in {size} bytes.\n'.format(path=self.filenames[0], size=path.getsize(self.filenames[0])))
+        sys.stdout.write('Succesfully wrote data to {path} in {size} bytes.\n'.format(path=self.filenames[1], size=path.getsize(self.filenames[1])))
 
     def exportConfig(self):
-        return
+        return None
     
     def exportData(self):
-        return
+        return None
