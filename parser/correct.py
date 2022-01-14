e23cd3d516d11b8f17483f7140ad8699f74d98b9
Jon Herbst <herbtaculargames@gmail.com>
2022-01-14 17:43:39 +0000

    Track pattern ID changes within FT export file to aid mapping of the pattern and row data

--- parser/correct.py
+++ parser/correct.py
@@ -12,6 +12,8 @@ class FixExport:
 
             try:
                 with open(self.filename, 'r+') as export_file:
+                    pattern_id = '00'
+
                     for line in export_file:
                         if line.startswith('ORDER'):
                             parts = line.split(':')
@@ -20,7 +22,13 @@ class FixExport:
                             pattern_register = pattern_register.lstrip()
                             pattern_register = pattern_register.split(' ')
 
-                            pattern_map[str(parts[0].replace('ORDER ', '').strip())] = {}
+                            pattern_map[str(parts[0].replace('ORDER ', '').strip())] = [{register : []} for register in pattern_register]
+                            """Generate empty lists to populate with individual channel data on a per-pattern basis."""
+                        elif line.startswith('PATTERN'):
+                            parts = line.split(' ')
+                            pattern_id = parts[1].rstrip('\n')
+                        elif line.startswith('ROW'):
+                            parts = line.split(':')
 
             except OSError as ex:
                 """Terminate if an invalid path has been provided."""
